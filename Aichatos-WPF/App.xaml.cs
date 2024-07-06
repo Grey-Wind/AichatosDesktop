1d08d2cb416edc1b3e8974fe3191a30737080cfc
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-07-06 22:31:50 +0800

    创建项目

---
+++ Aichatos-WPF/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Aichatos_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private Mutex? mutex;
+
+        public App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            mutex = new Mutex(true, "aichatos", out bool ret);
+
+            if (!ret)
+            {
+                Environment.Exit(0);
+                return;
+            }
+        }
+    }
+
+}
