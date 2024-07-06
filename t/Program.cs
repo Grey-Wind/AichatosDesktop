08c56d87d039842a515ca9724836ad180426fdf7
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-07-06 21:39:56 +0800

    创建t

---
+++ t/Program.cs
@@ -0,0 +1,25 @@
+﻿if (args.Length == 0)
+{
+    return;
+}
+
+string command = args[0];
+
+switch (command)
+{
+    case "greet":
+        Greet();
+        break;
+    case "time":
+        ShowTime();
+        break;
+    case "date":
+        ShowDate();
+        break;
+    case "exit":
+        Exit();
+        break;
+    default:
+        Console.WriteLine("Unknown command. Available commands: greet, time, date, exit.");
+        break;
+}
